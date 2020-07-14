dc2241102bc69fe199bfe0b9719a7e3c5cc42734
aquinlan82 <anq2@illinois.edu>
2020-07-14 07:21:33 -0500

    Modifications to allow calendars app to use smaller images

--- helpers.py
+++ helpers.py
@@ -20,12 +20,25 @@ def getServerConnection(windows):
 		conn = pyodbc.connect("Driver="+driver+";SERVER="+server+";DATABASE="+database+";USER="+username+";PASSWORD="+password+";")
 	return conn
 
+""" Determines how to get to images """
+def getImagePath(windows, small):
+	path = "images/"
+	if windows:
+		path = path + "/"
+	if small:
+		path = path + "smallV"
+	else:
+		path = path + "v"
+	path = path + "alidYears/"
+	if windows:
+		path = path + "/"
+	return path
 
-""" Finds image for the first year before the input year """
-def getImage(year, base):
+"""Determines image for first year before input year """
+def getAnyImage(year, base, small):
 	images = []
 	#collect all image files bigger than year
-	for root, dirs, files, in os.walk("images/validYears"):
+	for root, dirs, files, in os.walk(getImagePath(False, small)):
 		for file in files:
 			if file.endswith(".png"):
 				path = file.split(".")[0]
@@ -42,3 +55,11 @@ def getImage(year, base):
 		filename = base + str(images[-1]) + ".png"
 	img = Image.open(filename)
 	return img
+
+""" Finds big image for the first year before the input year """
+def getImage(year, base):
+	return getAnyImage(year, base, False)
+
+""" Finds small image for the first year before the input year """
+def getSmallImage(year, base):
+	return getAnyImage(year, base, True)
